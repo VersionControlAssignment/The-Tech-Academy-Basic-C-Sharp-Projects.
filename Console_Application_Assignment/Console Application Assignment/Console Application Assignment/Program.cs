69de0414e5ba43da01e20ff141204e8188b9884f
VersionControlAssignment <123970284+VersionControlAssignment@users.noreply.github.com>
2023-09-04 22:35:54 +0100

    Add files via upload

---
+++ Console_Application_Assignment/Console Application Assignment/Console Application Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Console_Application_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int product = 10 * 50;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int num1 = 3;
+            //int num2 = 25;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //int num1 = 100;
+            //decimal num2 = 12.5m;
+            //decimal total = num1 / num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 100 > 50;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int remainder = 70 % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
