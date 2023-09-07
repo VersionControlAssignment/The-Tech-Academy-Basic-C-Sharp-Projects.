bc0df35c48dd8d76174c80edc30396d1b50aea9c
VersionControlAssignment <123970284+VersionControlAssignment@users.noreply.github.com>
2023-09-07 14:00:44 +0100

    Add files via upload

---
+++ String Assignment/String Assignment/String Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jesse";
+            string quote = "Bob said, \"Hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            string fileName = "C:\\Users\\Jesse";
+
+            name = name.ToUpper();
+
+            Console.WriteLine(quote);
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+            //StringBuilder sb = new StringBuilder();
+
+            //sb.Append("My name is Jesse");
+
+            //Console.WriteLine(sb);
+            //Console.ReadLine();
+        }
+    }
+}
