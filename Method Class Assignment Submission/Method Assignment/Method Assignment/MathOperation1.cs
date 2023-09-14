c782e59092e6fe5e51456a8a131d73224dfedc47
VersionControlAssignment <123970284+VersionControlAssignment@users.noreply.github.com>
2023-09-14 23:31:20 +0100

    Add files via upload

---
+++ Method Class Assignment Submission/Method Assignment/Method Assignment/MathOperation1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Method_Assignment
+{
+    class MathOperation1
+    {
+        //Create a class. In that class, create a void method that takes two integers as parameters.
+        //Have the method do a math operation on the first integer and display the second integer to the screen. 
+        public int MathOperation(int i, int j = 1)
+        {
+            
+            int result = i * j;
+            return result;
+        }
+    }
+}
