5e6643a56c6cb77d357ded989fade3e8ca00b2a0
VersionControlAssignment <123970284+VersionControlAssignment@users.noreply.github.com>
2023-10-07 23:28:45 +0100

    Add files via upload

---
+++ Operators Assignment/Operators Assignment/Operators Assignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    //Create an Employee class with Id, FirstName and LastName properties. 
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
