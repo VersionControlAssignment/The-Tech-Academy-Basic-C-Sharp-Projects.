9dc723c01e7d5e838fed32c01ab842acf134f601
VersionControlAssignment <123970284+VersionControlAssignment@users.noreply.github.com>
2023-10-08 22:51:26 +0100

    Add files via upload

---
+++ Method Class Assignment/Method Class Assignment/Method Class Assignment/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+abstract class Motorcycle
+{
+    // Anyone can call this.
+    public void StartEngine() {/* Method statements here */ }
+
+    // Only derived classes can call this.
+    protected void AddGas(int gallons) { /* Method statements here */ }
+
+    // Derived classes can override the base class implementation.
+    public virtual int Drive(int miles, int speed) { /* Method statements here */ return 1; }
+
+    // Derived classes can override the base class implementation.
+    public virtual int Drive(TimeSpan time, int speed) { /* Method statements here */ return 0; }
+
+    // Derived classes must implement this.
+    public abstract double GetTopSpeed();
+}
